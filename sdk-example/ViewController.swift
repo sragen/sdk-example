3e94e1ca322f183c1e3a6041c80167df6566533b
adi.kurniawan <adi.kurniawan>
2025-01-09 17:19:29 +0700

    Initial Commit

---
+++ sdk-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sdk-example
+//
+//  Created by Bluebird Macbook on 09/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sdk-exampleUITests/sdk_exampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sdk_exampleUITestsLaunchTests.swift
+//  sdk-exampleUITests
+//
+//  Created by Bluebird Macbook on 09/01/25.
+//
+
+import XCTest
+
+final class sdk_exampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
