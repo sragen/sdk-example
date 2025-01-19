fad503060a5358d886afb7259c8d861782c7ecf6
Adi Kurniawan <>
2025-01-19 11:28:18 +0700

    import MyBirdtalk

--- sdk-example/SceneDelegate.swift
+++ sdk-example/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MyBirdtalk
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,12 +18,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-            let window = UIWindow(windowScene: windowScene)
-            let storyboard = UIStoryboard(name: "MainTinode", bundle: nil)
-            window.rootViewController = storyboard.instantiateInitialViewController()
-            self.window = window
-            window.makeKeyAndVisible()
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//            let window = UIWindow(windowScene: windowScene)
+//            let storyboard = UIStoryboard(name: "MainTinode", bundle: nil)
+//            window.rootViewController = storyboard.instantiateInitialViewController()
+//            self.window = window
+//            window.makeKeyAndVisible()
+        
+        let env = AppConfiguration.sharedInstance.environment
+        print("my environment \(env)")
+        
         
         //guard let _ = (scene as? UIWindowScene) else { return }
     }
